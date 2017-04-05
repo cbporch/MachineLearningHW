d4c86937823bc7c0222ac1a04837e611792f6f51
Christopher Porch <cbporch@gmail.com>
2017-04-05 01:01:38 -0400

    still needs testing

--- overfit4-4.py
+++ overfit4-4.py
@@ -1,5 +1,5 @@
 from numpy import arange, sqrt
-from numpy.polynomial import legendre
+from numpy.polynomial import legendre, polynomial as poly
 from numpy.random import uniform, randn
 
 Q_f = arange(1, 100)
@@ -11,13 +11,21 @@ def f(x, c):
     return legendre.legval(x=x, c=c)
 
 
+def g_2(x, y):
+    return poly.polyfit(x, y, 2)
+
+
+def g_10(x, y):
+    return poly.polyfit(x, y, 10)
+
+
 def run_experiments():
     for num in N:
         for dim in Q_f:
             for sig in sigma_squared:
                 # generate x values
-                x = uniform(-1, 1, (num, dim))
-                print(x)
+                k = num * 0.2
+                x = uniform(-1, 1, (num, 1))
 
                 # a_q are the coefficients for the Legendre target function,
                 # and are pulled from a standard normal distribution
@@ -28,10 +36,17 @@ def run_experiments():
                 for x_i in x:
                     y.append(f(x_i[0], a_q) + sqrt(sig) * randn())
 
-                # TODO: find g_2, linear regression
+                train_x = x[0:num - k], test_x = x[num - k:]
+                train_y = y[0:num - k], test_y = y[num - k:]
 
-                # TODO: calculate E_in for g_2
+                # g2_coeff and g10_coeff are coefficient matrices for a polynomial fit
+                g2_coeff = g_2(train_x, train_y)
+                g10_coeff = g_10(train_x, train_y)
 
-                # TODO: find g_10, linear regression
+                g2_sum = 0
+                for i in len(test_x):
+                    g2_sum += (poly.polyval(test_x[i], g2_coeff) - test_y[i]) ** 2
 
-                # TODO: calculate E_in for g_2
+                g10_sum = 0
+                for i in len(test_x):
+                    g10_sum += (poly.polyval(test_x[i], g10_coeff) - test_y[i]) ** 2
