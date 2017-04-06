3b6d973d54b73ad8d7b67f59ccd620341093cd56
Christopher Porch <cbporch@gmail.com>
2017-04-06 18:19:35 -0400

    more additions

--- nearest_neighbor.py
+++ nearest_neighbor.py
@@ -1,5 +1,42 @@
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 from sklearn import neighbors
+from sklearn.decomposition import PCA
 
+x_data = np.array([[0, 0], [0, 1], [5, 5]])
+y_data = np.array([1,1,-1])
+
+pca = PCA(n_components=1, whiten=True)
+x = pca.fit_transform(x_data, y_data)
+x = np.array([[x[0], 1], [x[1], 1], [x[2], 1]])
+print(x)
+step = .05
+
+cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
+
+clf = neighbors.KNeighborsClassifier(n_neighbors=2, weights='distance')
+clf.fit(x, y_data)
+
+# Plot the decision boundary. For that, we will assign a color to each
+# point in the mesh [x_min, x_max]x[y_min, y_max].
+x_min, x_max = -1, 2
+y_min, y_max = 0, 2
+xx, yy = np.meshgrid(np.arange(x_min, x_max, step),
+                     np.arange(y_min, y_max, step))
+print(xx.ravel())
+Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+
+# Put the result into a color plot
+Z = Z.reshape(xx.shape)
+plt.figure()
+plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+
+# Plot also the training points
+plt.scatter(x[:, 0], x[:, 1], c=y_data, cmap='RdBu')
+plt.xlim(xx.min(), xx.max())
+plt.ylim(yy.min(), yy.max())
+plt.title("NN classification, PCA (k = %i, weights = '%s')"
+          % (2, 'distance'))
+
+plt.show()
--- overfit4-4.py
+++ overfit4-4.py
@@ -1,77 +1,80 @@
+import cProfile
+
 import numpy as np
 from numpy.polynomial import legendre
 from numpy.polynomial.polynomial import polyfit, polyval
 from numpy.random import uniform, randn
-import cProfile
 
-Q_f = np.arange(1, 100)
+Q_f = np.arange(1, 100, 10)
 N = np.arange(20, 120, 5)
-sigma_squared = np.arange(0, 2, 0.05)
-experiment_size = 100
+sigma_squared = np.arange(0, 2, 0.5)
 
 
 def f(x, c):
-    return legendre.legval(x=x, c=c)
+    return sum(legendre.legval(x=x, c=c))
 
 
 def g_2(x, y):
-    return polyfit(x, y, 2)
+    return polyfit(x[:, 1], y, 2)
 
 
 def g_10(x, y):
-    return polyfit(x, y, 10)
+    return polyfit(x[:, 1], y, 10)
 
 
 def run_experiments():
     lo = hi = 1
-    exper_range = range(experiment_size)
-    # for num in N:                               # 100
-    num = 20
-    y = np.zeros(num)
-    print("num: {0}".format(num))
-    k = int(np.ceil(num * 0.2))
-    diff = num - k
-    # generate x values
-    x = uniform(-1, 1, num)
+    experiment_size = 100
 
-    for dim in Q_f:  # 20
-        print("\tQ_f dim: {0}".format(dim))
-        # a_q are the coefficients for the Legendre target function,
-        # and are pulled from a standard normal distribution
-        a_q = randn(dim)
+    for num in N:                               # 100
+        y = np.zeros(num)
+        print("num: {0}".format(num))
+        k = int(np.ceil(num * 0.2))
+        diff = num - k
 
-        # break into test and training sets
-        train_x, test_x = x[:diff], x[diff:]
-        train_y, test_y = y[:diff], y[diff:]
+        for dim in Q_f:  # 20
+            print("\tQ_f dim: {0}".format(dim))
+            # a_q are the coefficients for the Legendre target function,
+            # and are pulled from a standard normal distribution
+            a_q = randn(dim)
 
-        for sig in sigma_squared:  # 40
-            g2_sq_err = g10_sq_err = 0  # = 80,000 runs, 100 experiments each,
-            sq = np.sqrt(sig)
-            # generate y values
-            for i in range(len(x)):
-                y[i] = f(x[i], a_q) + sq * randn()
+            for sig in sigma_squared:  # 40
+                g2_sq_err = g10_sq_err = 0  # = 80,000 runs, 100 experiments each,
+                sq = np.sqrt(sig)
 
-            for count in exper_range:  # = 8,000,000 runs
-                # g2_coeff and g10_coeff are coefficient matrices for a polynomial fit
-                g2_coeff = g_2(train_x, train_y)
-                g10_coeff = g_10(train_x, train_y)
+                for count in range(experiment_size):  # = 8,000,000 runs
+                    # generate x values
+                    x = uniform(-1, 1, (num,1))
+                    ones = np.ones((num, 1))
+                    x = np.concatenate((ones, x), axis=1)
 
-                # calculate squared error for g2
-                g2_sq_err += sum((polyval(test_x[i], g2_coeff) - test_y[i]) ** 2 for i in range(k)) / k
+                    # generate y values
+                    for i in range(len(x)):
+                        y[i] = f(x[i], a_q) + sq * randn()
 
-                # calculate squared error for g10
-                g10_sq_err += sum((polyval(test_x[i], g10_coeff) - test_y[i]) ** 2 for i in range(k)) / k
+                    # break into test and training sets
+                    train_x, test_x = x[:diff], x[diff:]
+                    train_y, test_y = y[:diff], y[diff:]
 
-            H2 = g2_sq_err / experiment_size
-            H10 = g10_sq_err / experiment_size
-            overfit = H10 - H2
+                    #  g2_coeff and g10_coeff are coefficient matrices for a polynomial fit
+                    g2_coeff = g_2(train_x, train_y)
+                    g10_coeff = g_10(train_x, train_y)
 
-            if overfit < lo:
-                lo = overfit
-                print("\t\tfor sigma: {0:1.2f} -> Lo: H10 - H2: {1:.4f}".format(sig, overfit))
-            elif overfit > hi:
-                hi = overfit
-                print("\t\tfor sigma: {0:1.2f} -> Hi: H10 - H2: {1:.4f}".format(sig, overfit))
+                    # calculate squared error for g2
+                    g2_sq_err += sum((polyval(test_x[i][1], g2_coeff) - test_y[i]) ** 2 for i in range(k)) / k
 
+                    # calculate squared error for g10
+                    g10_sq_err += sum((polyval(test_x[i][1], g10_coeff) - test_y[i]) ** 2 for i in range(k)) / k
+
+                H2 = g2_sq_err / experiment_size
+                H10 = g10_sq_err / experiment_size
+                overfit = H10 - H2
+
+                if overfit < lo:
+                    lo = overfit
+                    print("\t\tfor sigma: {0:1.2f} -> Lo: H10 - H2: {1:.4f}".format(sig, overfit))
+                elif overfit > hi:
+                    hi = overfit
+                    print("\t\tfor sigma: {0:1.2f} -> Hi: H10 - H2: {1:.4f}".format(sig, overfit))
 
 cProfile.run('run_experiments()')
