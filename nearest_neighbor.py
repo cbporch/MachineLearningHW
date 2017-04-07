f437c91060ca8bf975bb97cdad38b03d36053686
Christopher Porch <cbporch@gmail.com>
2017-04-06 20:56:59 -0400

    more additions

--- nearest_neighbor.py
+++ nearest_neighbor.py
@@ -1,15 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
+from scipy.cluster.vq import whiten
 from sklearn import neighbors
 from sklearn.decomposition import PCA
+from sklearn.preprocessing import normalize
 
 x_data = np.array([[0, 0], [0, 1], [5, 5]])
 y_data = np.array([1,1,-1])
 
-pca = PCA(n_components=1, whiten=True)
-x = pca.fit_transform(x_data, y_data)
-x = np.array([[x[0], 1], [x[1], 1], [x[2], 1]])
+# pca = PCA(n_components=1, whiten=True)
+# x = pca.fit_transform(x_data, y_data)
+# x = np.array([[x[0], 1], [x[1], 1], [x[2], 1]])
+
+x = normalize(x_data)
+x = whiten(x)
 print(x)
 step = .05
 
@@ -20,11 +25,11 @@ clf.fit(x, y_data)
 
 # Plot the decision boundary. For that, we will assign a color to each
 # point in the mesh [x_min, x_max]x[y_min, y_max].
-x_min, x_max = -1, 2
-y_min, y_max = 0, 2
+x_min, x_max = min(x[:,0]) - 1, max(x[:,0]) + 1
+y_min, y_max = min(x[:, 1]) - 1, max(x[:, 1]) + 1
 xx, yy = np.meshgrid(np.arange(x_min, x_max, step),
                      np.arange(y_min, y_max, step))
-print(xx.ravel())
+
 Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
 
 # Put the result into a color plot
